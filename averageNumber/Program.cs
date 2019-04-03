398aaa0a6a83a6f28eaa7e2e9d6484f3a7615749
Paolo Moscoso <Paolo.Moscoso@Jalasoft.com>
2019-04-03 16:46:01 -0400

    build(backend): average number

--- averageNumber/Program.cs
+++ averageNumber/Program.cs
@@ -10,6 +10,26 @@ namespace averageNumber
     {
         static void Main(string[] args)
         {
+            string numberInput;
+            int number, result = 0, counter = 0;
+            float average = 0;
+            bool option;
+            do
+            {
+                counter++;
+                Console.WriteLine("Please enter a number");
+                numberInput = Console.ReadLine();
+                number = int.Parse(numberInput);
+                result = result + number;
+                Console.WriteLine("if do you want plus other number press y");
+                numberInput = Console.ReadLine();
+                option = (numberInput == "y") ? true : false; 
+            }
+            while (option);
+            Console.WriteLine("the average is :");
+            average = result / counter;
+            Console.WriteLine(average);
+            Console.ReadKey();
         }
     }
 }
